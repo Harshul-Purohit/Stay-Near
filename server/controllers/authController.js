2e7f090611fefb7b8f56fa8683d44147ca357ece
Harshul-Purohit <harshulpurohit@gmail.com>
2026-08-18 16:35:40 +0530

    prepare web for production

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -116,6 +116,8 @@ export const logout = async (req, res, next) => {
     res.cookie('token', 'none', {
       expires: new Date(Date.now() + 10 * 1000),
       httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
     });
 
     res.status(200).json({
